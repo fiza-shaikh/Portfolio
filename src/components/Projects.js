e862dde40f2c165e6612cdd4b86b3a65cd29c1dd
Fiza Shaikh <fizashaikh@Mac.lan>
2025-04-08 00:31:43 -0400

    Initial portfolio upload

---
+++ src/components/Projects.js
@@ -0,0 +1,76 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Smart Load Management",
+      description: "A Linux-based C++ application for EV charging session management, optimizing bookings and applying user incentives for efficient resource allocation. Utilized Git for version control to enhance collaboration and system reliability.",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Criminal Identification Using Facial Recognition",
+      description: "Developed a Python-based web application integrating LLMs, NLP, and CNN for real-time criminal identification using facial features. Designed modules for image/video surveillance and data management to enhance security measures.",
+      imgUrl: projImg2,
+    },
+    {
+      title: "RELISH: The Cooking Application",
+      description: "Designed an Android application using Java, Android Studio, and MySQL, allowing users to browse and select recipes based on available ingredients. Integrated an admin module for adding and managing recipes.",
+      imgUrl: projImg3,
+    },
+    
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p> Following projects showcase my skills and experience through real-world applications. Each project highlights key technologies and problem-solving approaches. These projects reflect my ability to develop efficient solutions, work with diverse technologies, and manage projects effectively.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p> ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
